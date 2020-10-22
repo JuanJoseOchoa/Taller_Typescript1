68f173859de296bb9a09a2c5098642fffe69315d
JuanJoseOchoa <60239291+JuanJoseOchoa@users.noreply.github.com>
2020-10-22 18:12:10 -0500

    Add files via upload

---
+++ Taller_Typescript1/tallerTypescript/dataCourses.ts
@@ -0,0 +1,12 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en equipos", "Sergio Velasquez", 3),
+  new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+  new Course("Física 2", "Luis Quiroga", 3),
+  new Course("Fisica 2 Experimental", "David Guzman", 1),
+  new Course("Álgebra Lineal", "Carolina Benedetti", 3),
+  new Course("Cálculo Integral", "Maricarmen Martinez ", 3),
+  new Course("Italiano 2", "Valeria Busnelli", 3),
+]
